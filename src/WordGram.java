b6a1986d48a6f4b76d3bbd20e32c521eb12db718
Michael Jiron <jmj51@duke.edu>
2018-09-23 19:15:03 -0400

    v 1.1

--- src/WordGram.java
+++ src/WordGram.java
@@ -4,6 +4,7 @@
  * @author MICHAEL JIRON
  *
  */
+import java.util.Random;
 
 public class WordGram {
 	
@@ -12,18 +13,18 @@ public class WordGram {
 	private int myHash;         // cached hash value
 
 	/**
-	 * Create WordGram (add comments)
-	 * @param source
-	 * @param start
-	 * @param size
+	 * Create WordGram, an object that represents a set of strings and its hashcode, as well as a single string representing the entire array
+	 * @param source this is a set of strings
+	 * @param start this is the first element k that would start the myWords array
+	 * @param size this is the size, or length of the intended WordGram
 	 */
 	public WordGram(String[] source, int start, int size) {
 		myWords = new String[size];
-		// TODO: initialize myWords and ...
 
 		for(int k = 0; k < size; k++){
 			myWords[k] = source[start + k];
 		}
+
 		myToString = null;
 		myHash = 0;
 	}
@@ -41,12 +42,13 @@ public class WordGram {
 	}
 
 	/**
-	 * Complete this comment
-	 * @return
+	 * This returns the length of the myWords array
+	 * @return the length of the myWords array
 	 */
 	public int length(){
 		// TODO: change this
-		return 0;
+
+		return myWords.length;
 	}
 
 
@@ -56,31 +58,51 @@ public class WordGram {
 			return false;
 		}
 
-	    // TODO: complete this method
+		WordGram wg = (WordGram) o;
+		if(this.myWords.length != wg.length()){
+			return false;
+		}
+
+		for(int k = 0; k < this.myWords.length; k++){
+			if(this.myWords[k] != wg.myWords[k]){
+				return false;
+			}
+		}
+
 		return true;
 	}
 
 	@Override
 	public int hashCode(){
-		// TODO: complete this method
+		if(myHash == 0){
+			int k = new Random().nextInt();
+			myHash = this.toString().length() + k;
+		}
 		return myHash;
 	}
 	
 
 	/**
-	 * Create and complete this comment
+	 * this method deletes the first element, shifts every other element to the left, and the adds a new string at the last position
+	 * of a WordGram object, creating a new one in the process
 	 * @param last is last String of returned WordGram
-	 * @return
+	 * @return the resulting WordGram object from the operations aforementioned
 	 */
 	public WordGram shiftAdd(String last) {
 		WordGram wg = new WordGram(myWords,0,myWords.length);
-		// TODO: Complete this method
+		for(int k = 1; k < myWords.length; k++){
+			wg.myWords[k] = this.myWords[k-1];
+		}
+		wg.myWords[wg.length()-1] = last;
+
 		return wg;
 	}
 
 	@Override
 	public String toString(){
-		// TODO: Complete this method	
+		if(myToString == null) {
+			myToString = String.join(" ", myWords);
+		}
 		return myToString;
 	}
 }
